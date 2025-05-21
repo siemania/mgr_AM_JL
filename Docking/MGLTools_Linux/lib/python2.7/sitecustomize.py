bcf02d9f7070c540676f1c41636b4315a4d80881
Tajgero <jasiek.latt@gmail.com>
2025-05-21 16:58:31 -0400

    Ponownie dodaje MGLTools_Linux

---
+++ Docking/MGLTools_Linux/lib/python2.7/sitecustomize.py
@@ -0,0 +1,13 @@
+mglroot = '/home/kali/Desktop/mgr_AM_JL/Docking/MGLTools_Linux'
+# specify mglroot here
+import sys, os
+path = os.path.join(mglroot, "MGLToolsPckgs")
+sys.path.append(path)
+
+from os import getenv
+if getenv('MGLPYTHONPATH'):
+    sys.path.insert(0, getenv('MGLPYTHONPATH'))
+    
+from Support.path import setSysPath
+setSysPath(path)
+#sys.path.insert(0,os.path.abspath('.'))
