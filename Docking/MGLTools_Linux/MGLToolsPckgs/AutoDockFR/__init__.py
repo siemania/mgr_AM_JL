f240b8d34428824095442822b2e61be4b30d2231
Tajgero Tigrisso <36379630+Tajgero@users.noreply.github.com>
2025-05-23 18:58:26 +0200

    Delete Docking/MGLTools_Linux directory

--- Docking/MGLTools_Linux/MGLToolsPckgs/AutoDockFR/__init__.py
+++
@@ -1,9 +0,0 @@
-FlagCheck = 1
-packageContainsVFCommands = 1
-
-CRITICAL_DEPENDENCIES = ['FlexTree', 'mglutil', 'numpy', 'AutoDockTools',
-                         'memoryobject', 'cAutoDock']
-
-NONCRITICAL_DEPENDENCIES = ['MMTK', 'MolKit', 'Pmw', 'ViewerFramework',
-                            'PyBabel', 'AutoDockSuite', 'DejaVu',
-                            'NetworkEditor', 'Vision', 'opengltk']
--- Docking/MGLTools_Linux/MGLToolsPckgs/cAutoDock/__init__.py
+++
@@ -1,4 +0,0 @@
-
-__MGLTOOLSVersion__ = '1-4alpha3'
-CRITICAL_DEPENDENCIES = ['MolKit', 'PyAutoDock', 'memoryobject']
-NONCRITICAL_DEPENDENCIES = []
